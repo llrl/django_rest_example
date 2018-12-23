2ed569ec12acfe03b07931f8b826d058fd61b974
akira <akira@mail.com>
2018-12-23 23:07:48 +0200

    done first lab

--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,18 @@ from django.urls import path
 from django.contrib.auth.views import logout_then_login
 
 from .views import LoginView
+from .views import EditUserView
 from .views import ListUserView
+from .views import DeteleUserView
 from .views import RegistrationView
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login_form'),
     path('login', LoginView.as_view(), name='login_form'),
-    path('registration', RegistrationView.as_view(), name='registration_form'),
+    path('create_user', RegistrationView.as_view(), name='create_user_form'),
     path('list_users', ListUserView.as_view(), name='list_users'),
+    path('list_users/<int:role_id>', ListUserView.as_view(), name='list_users'),
+    path('edit_user/<int:id>', EditUserView.as_view(), name='edit_user'),
+    path('delete_user/<int:id>', DeteleUserView.as_view(), name='delete_user'),
     path('logout', logout_then_login, name='logoutv')
 ]
--- main/views.py
+++ main/views.py
@@ -9,6 +9,7 @@ from .forms import RegistrationForm
 from .models import Role
 from .models import User
 
+
 class LoginView(TemplateView):
     template_name = 'login.html'
 
@@ -19,9 +20,9 @@ class LoginView(TemplateView):
         print(user)
         if user:
             login(request, user)
-            return HttpResponse("OK")
+            return redirect('/list_users')
         else:
-            return HttpResponse("Not found user with this login")    
+            return HttpResponse("Not found user with this login")
 
 
 class RegistrationView(TemplateView):
@@ -29,13 +30,19 @@ class RegistrationView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         user = RegistrationForm(request.POST)
+        to_set_role = request.POST['role']
         new_user = user.save(commit=False)
         new_user.set_password(new_user.password)
-        role = Role.objects.get(name='user')
+        role = Role.objects.get(name=to_set_role)
         new_user.save()
         new_user.roles.add(role)
         new_user.save()
         return HttpResponse("OK")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['roles'] = Role.objects.all()
+        return context
 
 
 class LogoutView(TemplateView):
@@ -46,11 +53,55 @@ class LogoutView(TemplateView):
         logout(request)
         return redirect('/')
 
+
 class ListUserView(TemplateView):
 
     template_name = 'user_list.html'
 
-    def get_context_data(self, **kwargs):
+    def get(self, request, role_id=None):
+        user = self.request.user
+        if user.is_authenticated:
+            return render(request, self.template_name, self.get_context_data(role_id=role_id))
+        return redirect('/login')
+
+    def get_context_data(self, role_id=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['users'] = User.objects.all()
+        user = self.request.user
+        context['profile'] = user
+        if role_id:
+            users = User.objects.filter(roles__id=role_id)
+        else:
+            users = User.objects.all() 
+        context['users'] = users
+        context['is_superuser'] = user.is_superuser
         return context
+
+class EditUserView(TemplateView):
+
+    template_name = 'edit.html'
+
+    def get(self, request, id):
+        if request.user.is_authenticated:
+            print(type(request.user))
+            print(request.user)
+            roles = request.user.roles.all()
+            if 'admin' in roles:
+                print("okey")
+        return HttpResponse("OK")
+
+    def get_context_data(self, **kwargs):
+        print(**kwargs)
+        return HttpResponse(**kwargs)
+        
+
+class DeteleUserView(TemplateView):
+
+    def get(self, request, id):
+        user = request.user
+        if not user.is_authenticated or not user.is_superuser:
+            return HttpResponse("Your are not auth as superuser")
+        user_to_del = User.objects.filter(id=id).first()
+        if not user_to_del:
+            return HttpResponse("User to delete not exist")
+        user_to_del.delete()
+        return HttpResponse("OK")
